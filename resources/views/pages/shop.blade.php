f660b497b05dc1de0d585dc31111285984e6cef6
Robert Delim <rdelim03@gmail.com>
2023-07-24 15:54:01 +0800

    ey

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SHOP | TEMPLATE</title>
+  <title>Shop Jandra | Products</title>
   <!-- Google Font -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
   rel="stylesheet">
