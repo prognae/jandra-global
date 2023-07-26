be9d6b1183a5e94996f7925a9a3ea0811487e9a9
Kyle Ivan Eduvane <hgfrsh@gmail.com>
2023-07-27 01:26:39 +0800

    change

--- resources/views/pages/new-admin/item-list.blade.php
+++ resources/views/pages/new-admin/item-list.blade.php
@@ -60,69 +60,7 @@
       <div class="layout-container">
         <!-- Menu -->
 
-        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-          <div class="app-brand demo">
-            <a href="index.html" class="app-brand-link">
-                <img src="{{ asset('assets/img/favicon/jandra.png') }}" class="image-size" alt="">
-
-
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-              <i class="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
-          </div>
-
-          <div class="menu-inner-shadow"></div>
-
-          <ul class="menu-inner py-1">
-            <!-- Dashboard -->
-            <li class="menu-item">
-              <a href="index.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
-                <div data-i18n="Analytics">Dashboard</div>
-              </a>
-            </li>
-            <!-- Products -->
-            <li class="menu-item active">
-              <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bx-calendar-event"></i>
-                <div data-i18n="Products">Products</div>
-              </a>
-              <ul class="menu-sub">
-                <li class="menu-item">
-                  <a href="products.html" class="menu-link">
-                    <div data-i18n="Basic Inputs">Item List</div>
-                  </a>
-                </li>
-                <li class="menu-item">
-                  <a href="input-item.html" class="menu-link">
-                    <div data-i18n="Input groups">New Item</div>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <!-- End Products -->
-            <!-- Events -->
-            <li class="menu-item ">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                  <i class="menu-icon tf-icons bx bx-calendar-event"></i>
-                  <div data-i18n="Products">Events</div>
-                </a>
-                <ul class="menu-sub">
-                  <li class="menu-item">
-                    <a href="events.html" class="menu-link">
-                      <div data-i18n="Basic Inputs">View Event</div>
-                    </a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="add-events.html" class="menu-link">
-                      <div data-i18n="Input groups">Add Event</div>
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <!-- End Events -->
-          </ul>
-        </aside>
+        
         <!-- / Menu -->
 
         <!-- Layout container -->
